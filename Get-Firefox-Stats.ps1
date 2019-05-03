ed2c77c1b29f773c08cafd01644bd8a1eb90048e
Paul Theriault <pauljt@misuse.co>
2019-05-03 12:04:34 +1000

    automate measuring of unique firefox processes

---
+++ Get-Firefox-Stats.ps1
@@ -0,0 +1,51 @@
+﻿# $all_foxes = Get-WmiObject Win32_Process | Where-Object {$_.name -eq "firefox.exe" -and $_.CommandLine.Contains("-contentproc")}
+
+
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$False,Position=0)]
+   [string]$processName
+)
+
+
+. C:\code\arewesandboxedyet\Sandbox-Utils.ps1
+
+$output_dir = "firefox"
+$MAIN_EXE = "firefox.exe"
+$PLUGIN_EXE = "plugin-container.exe"
+$parent_process = Get-WmiObject Win32_Process | Where-Object {$_.name -eq $MAIN_EXE -and -not $_.CommandLine.Contains("-contentproc")}
+$child_processes = Get-WmiObject Win32_Process | Where-Object {$_.ParentProcessId -eq $parent_process.ProcessId}
+
+[hashtable]$unique_processes = @{} 
+[hashtable]$results = @{} 
+
+
+$child_processes.ForEach({
+    $process_type = getProcessType($_)
+    Write-host $process_type
+    if(-not $unique_processes.ContainsKey($process_type)){
+        $unique_processes.Add($process_type,$_)
+    }
+})
+
+$unique_processes.keys.ForEach({
+    $unique_processes.Item($_).processId
+    $result = GetStatsForProcess -ProcessId $unique_processes.Item($_).processId
+    $results.Add($_, $result)
+})
+
+rmdir -Recurse $output_dir
+mkdir $output_dir
+
+$results.keys.ForEach({
+    $results.item($_) |ConvertTo-Json > $output_dir/$_.json
+})
+
+
+
+
+
+
+   
+   
+    
---
+++ Sandbox-Utils.ps1
@@ -0,0 +1,86 @@
+﻿#Sandbox-Utils
+#
+# Functions for testing process sandboxing
+# Author: Paul Theriault <pauljt@mozilla.com>
+
+function Do-Something {
+
+  param($Thing)
+
+  Write-Output "I did something to $Thing"
+
+  }
+
+# GetStatsForProcess
+# Generates a summary of sandbox restrictions for a given process
+#
+# $ProcessId pid of process to check
+function GetStatsForProcess($ProcessId){
+
+    $DEVICE_TEST_PATH = "\"
+    $FILE_TEST_PATH = "C:\ProgramData" #win32path
+    $REGISTRY_TEST_PATH ="\Registry\Machine\Software\Microsoft"
+    $NTOBJECTRMANAGER_PATH = "C:\code\sandbox-attacksurface-analysis-tools\bin\Release\NtObjectManager\NtObjectManager.psd1"
+
+
+    #ensure NtObjectManager module is loaded
+    $NtObjectManagerLoaded = Get-Module -Name NtObjectManager
+    if($NtObjectManagerLoaded -eq $null){
+        Write-Host "NtObjectManager Not FOund: attempting to load module..."
+        Import-Module $NTOBJECTRMANAGER_PATH
+    }
+
+    if($NtObjectManagerLoaded -eq $null){
+        Write-Host "NtObjectManager not present and couldnt be loaded, exiting..."
+        Quit
+    }
+
+
+    [hashtable]$Return = @{} 
+
+    $process = Get-NtProcess $ProcessId
+
+    Write-Host "Target process: $($process.Name) ($($process.Pid))"
+
+    Write-Host "Testing process migigations"
+    $token = Get-NtToken -Primary -pid $processId
+
+    # process details
+    Write-Host "Getting process $($processId) migigations"
+    $Return.integrity = ($token.IntegrityLevel)
+    $Return.mitigations = $process.Mitigations |Select-Object *
+    #todo get job restrictions
+    #todo SIDs, RIDS etc
+
+    # test access to windows objects
+    $Return.regkeys = Get-AccessibleKey $REGISTRY_TEST_PATH  -ProcessIds $processId -Recurse -AccessRights GenericWrite
+    $Return.files = Get-AccessibleFile -ProcessIds $processId -Win32Path $FILE_TEST_PATH -Recurse -Tokens $token  -AccessRights GenericWrite -DirectoryAccessRights GenericWrite
+    $Return.objects = Get-AccessibleObject -Recurse -AccessRights GenericWrite -ProcessIds $processId -Win32Path \ 
+    $Return.alpc =  Get-AccessibleAlpcPort -ProcessIds $processId
+    $Return.devices = Get-AccessibleDevice $DEVICE_TEST_PATH -Recurse -AccessRights GenericWrite -ProcessIds $processId
+    $Return.pipes = Get-AccessibleNamedPipe -ProcessIds $processId 
+    $Return.services = Get-AccessibleService -ProcessIds $processId
+    $Return.processes = Get-AccessibleProcess -ProcessIds $processId
+
+    ################  Check if process has the ability to spawn a new process
+    $Return.createProcess = $FALSE;
+    # Launch a cmd prompt with the token
+    $newProcess = New-Win32Process -Token $ptoken -CommandLine c:\Windows\System32\cmd.exe
+    if($newProcess){
+        $Return.createProcess = $TRUE;
+    }
+
+    Return $Return
+}
+
+
+function getProcessType($process){
+    $type = $process.CommandLine.split(" ")[-1]
+    if($type -ne "tab"){
+        return $type
+    }
+    else{
+        #TODO: get the sub-type (file, privileged, web extension, ) 
+        return $type
+    }
+}
